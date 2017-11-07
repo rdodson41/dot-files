480db87288011dea8aa0e02eea767274d6707dc5
Richard E. Dodson <richard.elias.dodson@gmail.com>
2017-11-06 22:00:23 -0500

    Complete curl, git / yadm aliases iff respective required commands exist

--- .bash/completion.bash
+++ .bash/completion.bash
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-complete -A hostname curl
+if which curl > /dev/null; then
+  complete -A hostname curl
+fi
 
-__git_complete g __git_main
-__git_complete y __git_main
+if type -t __git_complete > /dev/null; then
+  __git_complete g __git_main
+  __git_complete y __git_main
+fi
