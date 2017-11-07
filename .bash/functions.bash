93871e2d413046c0c778aed529fd5d22b1c767e9
Richard E. Dodson <richard.elias.dodson@gmail.com>
2017-11-06 21:51:00 -0500

    Define p() iff fzf exists in ~/.bash/functions.bash

--- .bash/functions.bash
+++ .bash/functions.bash
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-p()
-{
-  cd "$(find "${HOME}/Developer" -mindepth 1 -maxdepth 1 -type d | fzf --cycle --query="${1}" --select-1 "${@:2}")"
-}
+if which fzf > /dev/null; then
+  p()
+  {
+    cd "$(find "${HOME}/Developer" -mindepth 1 -maxdepth 1 -type d | fzf --cycle --query="${1}" --select-1 "${@:2}")"
+  }
+fi
