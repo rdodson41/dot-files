353334923acdfd91427f4252e08ea940cc47a5d9
Richard E. Dodson <richard.elias.dodson@gmail.com>
2017-11-06 22:06:02 -0500

    Reorganize jq arguments in ~/.bash/alias/git.bash

--- .bash/alias/git.bash
+++ .bash/alias/git.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 if [[ -f "${HOME}/.bash/alias/git.jq" ]] && which git jq > /dev/null; then
-  eval "$(git config --get-regexp "^alias[.]" | jq --from-file "${HOME}/.bash/alias/git.jq" --raw-input --raw-output)"
+  eval "$(git config --get-regexp "^alias[.]" | jq --raw-input --raw-output --from-file "${HOME}/.bash/alias/git.jq")"
 fi
