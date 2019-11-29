dfc7319da31878faf602f4345800d31a8883934a
Richard Dodson <richard.elias.dodson@gmail.com>
2019-11-29 08:27:54 -0800

    Reinitialize 'yadm' repository

--- .bash/alias/git.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ -f "${HOME}/.bash/alias/git.jq" ]] && which git jq > /dev/null; then
-  eval "$(git config --get-regexp "^alias[.]" | jq --raw-input --raw-output --from-file "${HOME}/.bash/alias/git.jq")"
-fi
--- .bash/functions.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if which fzf > /dev/null; then
-  p()
-  {
-    cd "$(find "${HOME}/Developer" -mindepth 1 -maxdepth 1 -type d | fzf --cycle --query="${1}" --select-1 "${@:2}")"
-  }
-fi
